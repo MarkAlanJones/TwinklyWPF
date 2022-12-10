50891e17f61579325ccad8a5fe7e5a192536765e
Mark A Jones <Mark.Alan.Jones@outlook.com>
2022-12-10 18:24:55 -0500

    Move Controls into a seperate usercontrol

---
+++ TwinklyWPF/Panels/OGControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TwinklyWPF.Panels
+{
+    /// <summary>
+    /// Interaction logic for OGControl.xaml
+    /// </summary>
+    public partial class OGControl : UserControl
+    {
+        public OGControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
