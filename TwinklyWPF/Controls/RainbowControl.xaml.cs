ff886361e9a1a85e01da5bfe224f888a33467f3a
Mark A Jones <Mark.Alan.Jones@outlook.com>
2022-12-20 10:25:55 -0500

    Nicer looking rainbow dial with hue gradient

--- TwinklyWPF/Controls/RainbowControl.xaml.cs
+++ TwinklyWPF/Controls/RainbowControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+using TwinklyWPF.Util;
+using WpfRotaryControl;
 
 namespace TwinklyWPF.Controls
 {
@@ -10,6 +14,22 @@ namespace TwinklyWPF.Controls
         public RainbowControl()
         {
             InitializeComponent();
+
+            // Rainbow around the dial
+            var newsegments = new List<RotaryControlSegment>();
+            for (var i = 0; i < 360; i++)
+            {
+                newsegments.Add(new RotaryControlSegment()
+                {
+                    Fill = new SolidColorBrush(
+                        HSBColor.FromHSB(
+                            new HSBColor((float)((i / 360.0) * 255), 255, 255))),
+                    AngleInDegrees = 1
+                });
+
+            }
+
+            _dialRainbow.Segments = newsegments;
         }
     }
 }
