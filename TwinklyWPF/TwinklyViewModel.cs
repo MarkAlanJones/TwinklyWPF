acb8cb508d6625a94278561aa9d8213730b4f05c
Mark A Jones <Mark.Alan.Jones@outlook.com>
2023-12-24 14:33:29 -0500

    remove unused using

--- TwinklyWPF/TwinklyViewModel.cs
+++ TwinklyWPF/TwinklyViewModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using Twinkly.Fox;
